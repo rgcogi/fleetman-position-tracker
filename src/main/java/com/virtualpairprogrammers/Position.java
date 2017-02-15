402150ac37465963820e176f96962fcc279de4ea
rgcogi <rgcogi@gmail.com>
2017-02-15 07:48:12 -0700

    Update Position.java

--- src/main/java/com/virtualpairprogrammers/Position.java
+++ src/main/java/com/virtualpairprogrammers/Position.java
@@ -5,7 +5,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+ 
 import javax.xml.bind.annotation.XmlRootElement;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
